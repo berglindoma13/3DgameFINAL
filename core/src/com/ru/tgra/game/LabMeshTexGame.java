4ec3f796a62a1bdb99c456e3346a6f14307f92e5
berglindoma13 <berglindoma13@ru.is>
2016-11-02 15:29:05 +0000

    double stuff

--- core/src/com/ru/tgra/game/LabMeshTexGame.java
+++ core/src/com/ru/tgra/game/LabMeshTexGame.java
@@ -251,44 +251,44 @@ public class LabMeshTexGame extends ApplicationAdapter implements InputProcessor
 		//Gdx.gl.glBlendFunc(GL20.GL_ONE, GL20.GL_ONE);
 		//Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE);
 
-			//Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-			cam.perspectiveProjection(fov, (float)Gdx.graphics.getWidth() / (float)(2*Gdx.graphics.getHeight()), 0.2f, 2000.0f);
-			shader.setViewMatrix(cam.getViewMatrix());
-			shader.setProjectionMatrix(cam.getProjectionMatrix());
-			shader.setEyePosition(cam.eye.x, cam.eye.y, cam.eye.z, 1.0f);
+		//Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.perspectiveProjection(fov, (float)Gdx.graphics.getWidth() / (float)(2*Gdx.graphics.getHeight()), 0.2f, 2000.0f);
+		shader.setViewMatrix(cam.getViewMatrix());
+		shader.setProjectionMatrix(cam.getProjectionMatrix());
+		shader.setEyePosition(cam.eye.x, cam.eye.y, cam.eye.z, 1.0f);
 
-			ModelMatrix.main.loadIdentityMatrix();
-
-
-			float s = (float)Math.sin((angle / 2.0) * Math.PI / 180.0);
-			float c = (float)Math.cos((angle / 2.0) * Math.PI / 180.0);
-
-			//shader.setLightPosition(0.0f + c * 3.0f, 5.0f, 0.0f + s * 3.0f, 1.0f);
-			shader.setLightPosition(3.0f, 4.0f, 0.0f, 1.0f);
-			//shader.setLightPosition(cam.eye.x, cam.eye.y, cam.eye.z, 1.0f);
+		ModelMatrix.main.loadIdentityMatrix();
 
 
-			float s2 = Math.abs((float)Math.sin((angle / 1.312) * Math.PI / 180.0));
-			float c2 = Math.abs((float)Math.cos((angle / 1.312) * Math.PI / 180.0));
+		float s = (float)Math.sin((angle / 2.0) * Math.PI / 180.0);
+		float c = (float)Math.cos((angle / 2.0) * Math.PI / 180.0);
 
-			shader.setSpotDirection(s2, -0.3f, c2, 0.0f);
-			//shader.setSpotDirection(-cam.n.x, -cam.n.y, -cam.n.z, 0.0f);
-			shader.setSpotExponent(0.0f);
-			shader.setConstantAttenuation(1.0f);
-			shader.setLinearAttenuation(0.00f);
-			shader.setQuadraticAttenuation(0.00f);
+		//shader.setLightPosition(0.0f + c * 3.0f, 5.0f, 0.0f + s * 3.0f, 1.0f);
+		shader.setLightPosition(3.0f, 4.0f, 0.0f, 1.0f);
+		//shader.setLightPosition(cam.eye.x, cam.eye.y, cam.eye.z, 1.0f);
 
-			//shader.setLightColor(s2, 0.4f, c2, 1.0f);
-			shader.setLightColor(1.0f, 1.0f, 1.0f, 1.0f);
-			
-			shader.setGlobalAmbient(0.7f, 0.7f, 0.7f, 1);
 
-			//shader.setMaterialDiffuse(s, 0.4f, c, 1.0f);
-			shader.setMaterialDiffuse(1.0f, 1.0f, 1.0f, 1.0f);
-			shader.setMaterialSpecular(1.0f, 1.0f, 1.0f, 1.0f);
-			//shader.setMaterialSpecular(0.0f, 0.0f, 0.0f, 1.0f);
-			shader.setMaterialEmission(0, 0, 0, 1);
-			shader.setShininess(50.0f);
+		float s2 = Math.abs((float)Math.sin((angle / 1.312) * Math.PI / 180.0));
+		float c2 = Math.abs((float)Math.cos((angle / 1.312) * Math.PI / 180.0));
+
+		shader.setSpotDirection(s2, -0.3f, c2, 0.0f);
+		//shader.setSpotDirection(-cam.n.x, -cam.n.y, -cam.n.z, 0.0f);
+		shader.setSpotExponent(0.0f);
+		shader.setConstantAttenuation(1.0f);
+		shader.setLinearAttenuation(0.00f);
+		shader.setQuadraticAttenuation(0.00f);
+
+		//shader.setLightColor(s2, 0.4f, c2, 1.0f);
+		shader.setLightColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+		shader.setGlobalAmbient(0.7f, 0.7f, 0.7f, 1);
+
+		//shader.setMaterialDiffuse(s, 0.4f, c, 1.0f);
+		shader.setMaterialDiffuse(1.0f, 1.0f, 1.0f, 1.0f);
+		shader.setMaterialSpecular(1.0f, 1.0f, 1.0f, 1.0f);
+		//shader.setMaterialSpecular(0.0f, 0.0f, 0.0f, 1.0f);
+		shader.setMaterialEmission(0, 0, 0, 1);
+		shader.setShininess(50.0f);
 
 
 	}
@@ -296,36 +296,18 @@ public class LabMeshTexGame extends ApplicationAdapter implements InputProcessor
 	public void displaygame(){
 
 		//drawing the plane
-		ModelMatrix.main.pushMatrix();
 		airplane.display(shader);
 		airplaneModel.draw(shader);
-		ModelMatrix.main.popMatrix();
 
-		//draw the ring
+		//draw the rings
 		gates.display(shader);
 
-			//drawing the plane
-
-            ModelMatrix.main.pushMatrix();
-            //menu.display(welcome);
-			airplane.display(shader);
-			airplaneModel.draw(shader);
-			//SphereGraphic.drawSolidSphere(shader, tex);
-			ModelMatrix.main.popMatrix();
-
-
-			//draw the ring
-
-			gates.display(shader);
-
-			obstacle.display(shader);
-
-			//draw the environment
-			drawWorld();
-            //menu.display(shader,welcome);
+		//draw obstacles
+		obstacle.display(shader);
 
 		//draw the environment
 		drawWorld();
+
 	}
 
 
